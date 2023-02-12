b257f5fbb93af71847bfc9262e728aee6bb24dda
“Salil48” <salilchharia@gmail.com>
2023-02-12 19:24:44 +0530

    Wage For 20 Working Days

--- employee.js
+++ employee.js
@@ -4,7 +4,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8
 const WAGE_PER_HOURS=20;
-
+const NUM_WORKING_DAYS=20;
 
 function getWorkingHours(empCheck)
 {
@@ -23,7 +23,10 @@ function getWorkingHours(empCheck)
 }
 
 let empHrs=0;
+for(let day=0;day<NUM_WORKING_DAYS;day++)
+{
 let empCheck=Math.floor(Math.random()*10)%3;
-empHrs= getWorkingHours(empCheck);
+empHrs+=getWorkingHours(empCheck);
+}
 let empWage=empHrs*WAGE_PER_HOURS;
-console.log("emp Wage :"+empWage);
+console.log("Total Hrs : " +empHrs+ " Emp Wage : " +empWage);
