0a609dc0306dd14207f4785d663b2414635f00b6
“Salil48” <salilchharia@gmail.com>
2023-02-12 19:29:01 +0530

    Total Working Hours = 160

--- employee.js
+++ employee.js
@@ -5,28 +5,31 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8
 const WAGE_PER_HOURS=20;
 const NUM_WORKING_DAYS=20;
+const MAX_HRS_IN_MONTH=160;
 
 function getWorkingHours(empCheck)
 {
     switch(empCheck)
     {
-      case IS_PART_TIME:
-          return PART_TIME_HOURS;        
-            
-      case IS_FULL_TIME:
-          return FULL_TIME_HOURS;  
-
-          default:
-             return 0;       
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        
+        default:
+            return 0;
     }
-    
 }
 
-let empHrs=0;
-for(let day=0;day<NUM_WORKING_DAYS;day++)
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<=NUM_WORKING_DAYS)
 {
+    totalWorkingDays++;
 let empCheck=Math.floor(Math.random()*10)%3;
-empHrs+=getWorkingHours(empCheck);
+totalEmpHrs+=getWorkingHours(empCheck);
 }
-let empWage=empHrs*WAGE_PER_HOURS;
-console.log("Total Hrs : " +empHrs+ " Emp Wage : " +empWage);
+let empWage=totalEmpHrs*WAGE_PER_HOURS;
+console.log("Total Days : " +totalWorkingDays+ " Total Hrs : " +totalEmpHrs+ " Emp Wage : " +empWage);
