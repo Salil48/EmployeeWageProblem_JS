a126bb713afc34ee014e2fd339047842573b6fd6
“Salil48” <salilchharia@gmail.com>
2023-02-13 12:37:34 +0530

    UC13_Regex Pattern To Check Name

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -21,19 +21,30 @@ class EmployeePayrollData
 
     set name(name)
     {
-        this._name=name;
+        let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if(nameRegex.test(name))
+        this._name=name
+        else
+        throw 'Name is Incorrect';
     }
 
     toString()
     {
-        const options ={year:'numeric',month:'long',day:'numeric'}
+        const options = {year:'numeric',month:'long',day:'numeric'}
         const empDate=this.startDate===undefined?"undefined":
         this.startDate.toLocaleDateString("en-US",options)
-        return "id = "+this.id+" , name = "+this.name+" , salary = "+this.salary+", gender ="+this.gender+", startDate= "+empDate;
+        return "id = "+this.id+" , name = "+this.name+" , salary = "+this.salary+", gender = "+this.gender+", startDate ="+empDate;
     }
 }
 
-let employeePayrollData=new EmployeePayrollData(1,"Mark",4000,'M',new Date())
+let employeePayrollData=new EmployeePayrollData(1,"Mark",4000,"M",new Date())
 
 console.log(employeePayrollData.toString());
 
+try{
+    employeePayrollData.name="john";
+    console.log(employeePayrollData.toString());
+}catch(e)
+{
+    console.error(e);
+}
